baa7c983f73dc515e73c0a97fe6bca5f29684868
SouzasGabriele <souzagabriele.mns@gmail.com>
2025-11-08 14:04:39 -0400

    Primeiro Versão do Código

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,9 +1,61 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>   //Bibliotecas ESP32
+#include <analogWrite.h>
+#include <ESP32Tone.h>
+#include <ESP32PWM.h>
 
-void setup() {
-  // O seu código de inicialização vai aqui, para ser executado uma vez:
+#include <BLEController.h>
+
+#define leftMotorPin 25   //Pino Motor Esquerdo
+#define rightMotorPin 26  //Pino Motor Direito
+
+Servo MotorEsquerdo;
+Servo MotorDireito;
+
+
+void setup() 
+{
+  Serial.begin(115200);
+  controller.begin();
+
+  while(!controller.isConnected())    //Se Controle não estiver conectado vai printar isso
+  {
+    Serial.println("Esperando Conexão");
+    delay(250);
+  }
+
+  MotorEsquerdo.attach(leftMotorPin);
+  MotorDireito.attach(rightMotorPin);
+  MotorEsquerdo.write(90);
+  MotorDireito.write(90);
+
+  Serial.println("Conexão Estabelecida");    //Se Controle estiver conectado vai printar isso
 }
 
-void loop() {
-  // O seu código principal vai aqui, para ser executado repetidamente:
+void loop() 
+{
+  if (controller.isConnected()) 
+  {
+    int RStickX = e.rightStickX;    //Vai andar ao usar o Joystick Esquerdo
+    int RStickY = e.rightStickY;
+
+    int leftMotorOutput  = constrain(map(LStickY + LStickX, -127, 127, 0, 180), 0, 180); //Mapear os valores 
+    int rightMotorOutput = constrain(map(LStickY - LStickX, -127, 127, 0, 180), 0, 180);
+
+    if (abs(LStickY) > 15 || abs(LStickX) > 15) 
+    {
+      MotorEsquerdo.write(leftMotorOutput);
+      MotorDireito.write(rightMotorOutput);
+    } 
+    else 
+    {
+      MotorEsquerdo.write(90);
+      MotorDireito.write(90);
+    }
+  }
+
+
+  Serial.print(leftMotorOutput);    //Verificação se os valores de Saída estão corretos
+  Serial.print(" ");
+  Serial.println(rightMotorOutput);
 }
